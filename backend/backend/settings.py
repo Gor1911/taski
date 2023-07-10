11e9f9910149fa355d9b45202142482ba7aca0a8
root <root@epdqsn98sef63r2i4ecd.auto.internal>
2023-07-10 03:40:48 +0000

    123

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,13 @@
 from pathlib import Path
-
+															
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['62.84.120.229', '127.0.0.1', 'localhost', 'mytaskee.ddns.net']
 
 
 # Application definition
@@ -103,7 +103,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
